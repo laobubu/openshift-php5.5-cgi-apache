2cdb1d01f83724076f3cf247a67f0e5830931467
laobubu <laobubu@gmail.com>
2015-07-08 19:03:35 +0800

    Add missing OPENSHIFT_RUNTIME_DIR

--- misc/common.sh
+++ misc/common.sh
@@ -2,4 +2,6 @@
 
 export HTTPD_ARGUMENT="-f ${OPENSHIFT_REPO_DIR}/conf/httpd.conf"
 
+export OPENSHIFT_RUNTIME_DIR=${OPENSHIFT_HOMEDIR}/app-root/runtime
+
 env|>${OPENSHIFT_TMP_DIR}/httpd_temp.conf awk 'BEGIN{FS="="} $1 ~ /^OPENSHIFT/ {print "PassEnv", $1}'
